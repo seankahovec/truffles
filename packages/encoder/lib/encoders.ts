sukanyaparashar/truffle

Move ProviderAdapter from decoder to encoder

packages/encoder/lib/encoders.ts
@@ -8,8 +8,8 @@ import type * as Abi from "@truffle/abi-utils";
 import * as Types from "./types";
 import Big from "big.js";
 import type { Provider } from "web3/providers";
-import Web3 from "web3";
 import Web3Utils from "web3-utils";
+import { ProviderAdapter, Eip1193Provider } from "./adapter";
 import * as Utils from "./utils";
 import {
   NoInternalInfoError,
@@ -38,7 +38,7 @@ interface ENSCache {
  * @category Encoder
  */
 export class ProjectEncoder {
-  private provider: Provider | null;
+  private provider: Provider | Eip1193Provider | null;
   private ens: any | null; //any should be ENS, sorry >_>
   private registryAddress: string | undefined = undefined;
   private ensCache: ENSCache = {};
@@ -124,7 +124,7 @@ export class ProjectEncoder {
         //but what is that?  We have to look it up.
         //NOTE: ENS is supposed to do this for us in the constructor,
         //but due to a bug it doesn't.
-        const networkId = await new Web3(this.provider).eth.net.getId();
+        const networkId = await new ProviderAdapter(this.provider).getNetworkId();
         const registryAddress: string | undefined = getEnsAddress(networkId);
         if (registryAddress) {
           this.ens = new ENS({

packages/encoder/lib/index.ts
@@ -168,8 +168,11 @@ export {
   ContractNotDeployedError
 } from "./errors";
 
+import { ProviderAdapter } from "./adapter";
+export { ProviderAdapter };
+export { Eip1193Provider } from "./adapter";
+
 import { Compilations } from "@truffle/codec";
-import Web3 from "web3";
 
 /**
  * @hidden
@@ -400,7 +403,7 @@ async function networkIdForInfo(
   if (settings.networkId !== undefined) {
     return settings.networkId;
   } else if (settings.provider) {
-    return await new Web3(settings.provider).eth.net.getId();
+    return await new ProviderAdapter(settings.provider).getNetworkId();
   } else {
     return null;
   }

packages/encoder/lib/types.ts
@@ -1,4 +1,5 @@
 import type { Provider } from "web3/providers";
+import type { Eip1193Provider } from "./adapter";
 import type { ContractObject as Artifact } from "@truffle/contract-schema/spec";
 import type { Compilations, Format, Evm } from "@truffle/codec";
 import type * as Codec from "@truffle/codec";
@@ -48,7 +49,7 @@ export interface EncoderInfoInternal {
   allocations?: Evm.AllocationInfo;
   compilations?: Compilations.Compilation[];
   networkId?: number | null;
-  provider?: Provider | null;
+  provider?: Provider | Eip1193Provider | null;
   registryAddress?: string;
 }
 
@@ -85,7 +86,7 @@ export interface EncoderSettings {
    * Including this will also turn on ENS resolution unless it is turned off in
    * the ENS settings (see below).
    */
-  provider?: Provider;
+  provider?: Provider | Eip1193Provider;
   /**
    * Optionally include a network ID; this is used for the same purposes as the
    * provider (see above), but won't turn on ENS resolution.
@@ -116,7 +117,7 @@ export interface EnsSettings {
    * ENS resolution.  If absent, will default to the encoders's usual provider,
    * if there is one, or to `null`, if not.
    */
-  provider?: Provider | null;
+  provider?: Provider | Eip1193Provider | null;
   /**
    * The ENS registry address to use; if absent, will use the default one
    * for the current network.  If there is no default registry for the

seankahovec/truffles

packages/encoder/lib/encoders.ts
@@ -7,9 +7,8 @@ import * as Codec from "@truffle/codec";
 import * as Abi from "@truffle/abi-utils";
 import * as Types from "./types";
 import Big from "big.js";
-import type { Provider } from "web3/providers";
-import Web3 from "web3";
 import Web3Utils from "web3-utils";
+import { ProviderAdapter, Provider } from "./adapter";
 import * as Utils from "./utils";
 import {
   NoInternalInfoError,

packages/encoder/lib/index.ts
@@ -168,8 +168,11 @@ export {
   ContractNotDeployedError
 } from "./errors";
 
+import { ProviderAdapter } from "./adapter";
+export { ProviderAdapter };
+export { Provider } from "./adapter";
+
 import { Compilations } from "@truffle/codec";
-import Web3 from "web3";
 
 /**
  * @hidden
@@ -400,7 +403,7 @@ async function networkIdForInfo(
   if (settings.networkId !== undefined) {
     return settings.networkId;
   } else if (settings.provider) {
-    return await new Web3(settings.provider).eth.net.getId();
+    return await new ProviderAdapter(settings.provider).getNetworkId();
   } else {
     return null;
   }

packages/encoder/lib/types.ts
@@ -1,4 +1,4 @@
-import type { Provider } from "web3/providers";
+import type { Provider } from "./adapter";
 import type { ContractObject as Artifact } from "@truffle/contract-schema/spec";
 import type { Compilations, Format, Evm } from "@truffle/codec";
 import type * as Codec from "@truffle/codec";